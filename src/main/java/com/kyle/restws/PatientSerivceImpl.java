2950059915d9c09711ae2dbec19aaae55b44fb63
Kyle <kderrick1988@gmail.com>
2018-09-09 13:44:06 -0700

    add Patient classes with annotations

---
+++ src/main/java/com/kyle/restws/PatientSerivceImpl.java
@@ -0,0 +1,8 @@
+package com.kyle.restws;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PatientSerivceImpl implements PatientService {
+
+}
---
+++ src/main/java/com/kyle/restws/PatientService.java
@@ -0,0 +1,8 @@
+package com.kyle.restws;
+
+import javax.ws.rs.Path;
+
+@Path("/patientservice")
+public interface PatientService {
+
+}
