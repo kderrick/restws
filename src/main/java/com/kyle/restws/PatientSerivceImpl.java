5199bb7bfa2ff72440a7e30da57c7c444f621ff1
Kyle <kderrick1988@gmail.com>
2018-09-10 16:36:54 -0700

    implement init and constructor for PatientSerivceImpl

--- src/main/java/com/kyle/restws/PatientSerivceImpl.java
+++ src/main/java/com/kyle/restws/PatientSerivceImpl.java
@@ -1,8 +1,26 @@
 package com.kyle.restws;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
+
+import com.kyle.restws.model.Patient;
 
 @Service
 public class PatientSerivceImpl implements PatientService {
 
+	Map<Long, Patient> patients = new HashMap<>();
+	long currentId = 123;
+	
+	public PatientSerivceImpl() {
+		init();
+	}
+	
+	void init() {
+		Patient patient = new Patient();
+		patient.setId(currentId);
+		patient.setName("John");
+		patients.put(patient.getId(), patient);
+	}
 }
