37b7bcbaff0d99e5812f39831684ffdd0ef62180
Kyle <kderrick1988@gmail.com>
2018-09-22 08:37:53 -0700

    configure to produce xml or json

--- src/main/java/com/kyle/restws/PatientService.java
+++ src/main/java/com/kyle/restws/PatientService.java
@@ -2,16 +2,19 @@ package com.kyle.restws;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import com.kyle.restws.model.Patient;
-
+@Consumes("application/xml,application/json")
+@Produces("application/xml,application/json")
 @Path("/patientservice")
 public interface PatientService {
 	
